6b340aaf5ea9f6f6b534090f02a8bd65c83c5c0e
Andreas <andreas.jonasson@gmail.com>
2022-11-20 18:40:46 +0100

    Started on a class that toggles a pin on and off every 0.5s.

--- src/main/java/se/drutt/greenhouse/controller/BlinkPin.java
+++ src/main/java/se/drutt/greenhouse/controller/BlinkPin.java
@@ -1,5 +1,46 @@
 package se.drutt.greenhouse.controller;
 
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalState;
+
 public class BlinkPin
 {
+    private final DigitalOutput output;
+    private boolean timeToStop;
+
+    public BlinkPin(Context pi4j, int pin)
+    {
+        this.output = pi4j.dout().create(pin);
+        output.state(DigitalState.HIGH);
+    }
+
+    public void start()
+    {
+        timeToStop = false;
+        ToggleThread toggleThread = new ToggleThread();
+        toggleThread.start();
+    }
+
+    public void stop()
+    {
+        timeToStop = true;
+    }
+
+    public String toString()
+    {
+        return "CURRENT DIGITAL OUTPUT [" + output + "] STATE IS [" + output.state() + "]";
+    }
+
+    private class ToggleThread extends Thread
+    {
+        public void run()
+        {
+            while (!timeToStop)
+            {
+                try { sleep(500); } catch (InterruptedException e) { throw new RuntimeException(e); }
+                output.toggle();
+            }
+        }
+    }
 }
